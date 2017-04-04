79ec0a720bd3e1757d4bb65554b55f4961044afa
Margarita Bartholomew <margarita.veracruz@gmail.com>
2017-04-04 09:48:57 -0400

    add puts statements to get output

--- coffee-and-code3.rb
+++ coffee-and-code3.rb
@@ -1,13 +1,17 @@
 # Question 1: ARRAY MAP
 array = (0..7).map { |x| 12 ** x }
 
+puts array.inpsect
+
 # Question 2: REVERSALS
 words = ['first', 'second', 'third', 'fourth', 'fifth', 'sixth']
 
 words.map! { |word| word.reverse}
-words.reverse
+words.reverse!
+
+puts words.inspect
 
 # Question 3: ARRAY TO HASH
 movies = [['Alfonso Cuaron', 'Gravity'], ['Spike Jonze', 'Her'], ['Martin Scorsese', 'The Wolf of Wall Street']]
 
-movies.to_h
+puts movies.to_h
